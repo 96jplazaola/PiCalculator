93ee51d686b4a766538be8db054c34703bc1bd34
96jplazaola <Ibargarai182b!>
2017-02-22 12:01:16 +0100

    CompletionServiceProgram

--- src/p3calculodepi/CalculadorPi.java
+++ src/p3calculodepi/CalculadorPi.java
@@ -1,33 +1,66 @@
 package p3calculodepi;
 
+import java.util.concurrent.*;
+
 public class CalculadorPi {
-	final static double REFERENCIAPI = 3.14159265358979323846;
+	final static double REFERENCIAPI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628;
+	final static long INTERVALOS = 10000000000L;
+	final static long TASKS = 10000;
+	final static Long INDEX = INTERVALOS / TASKS;
+
 	private long numIntervalos;
 	private double valorPi;
-	private double anchuraIntervalo;
-	
-	public CalculadorPi(long numIntervalos){
+	public static double anchuraIntervalo;
+	static ExecutorService executor;
+	private static CompletionService<Double> completionService;
+
+	public CalculadorPi(long numIntervalos) {
 		this.numIntervalos = numIntervalos;
-		anchuraIntervalo = 1.0/numIntervalos;
+		anchuraIntervalo = 1.0 / numIntervalos;
 	}
 
-	public double calcularPi(){
-		valorPi = 0;
-		double x;
-		for (long i = 0; i<numIntervalos; i++){
-			x= (i+0.5)*anchuraIntervalo;
-			valorPi += 4/(1+(x*x));
-		}
-		valorPi *= anchuraIntervalo;
-		return valorPi;
+
+	public static double calcularPartePi(Long i) {
+		double x = (i + 0.5) * anchuraIntervalo;
+		return 4 / (1 + (x * x));
 	}
-	public static void main (String args[]){
-		CalculadorPi programa = new CalculadorPi(1000000000);
+
+	public double inicializarThreads() {
+		PiCalculatorThread thread;
+		valorPi = 0.0;
+		executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+		completionService = new ExecutorCompletionService<Double>(executor);
+		for (long i = 0; i < TASKS; i++) {
+			completionService.submit(new PiCalculatorThread(INDEX * i, INDEX * (i + 1)));
+		}
+		for (long i = 0; i < TASKS; i++) {
+			Double valor;
+			Future<Double> future;
+			try {
+				future = completionService.take();
+				if ((valor = future.get()) != null) {
+					valorPi += valor;
+				}
+
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+
+		}
+		executor.shutdown();
+		return valorPi * anchuraIntervalo;
+	}
+
+	public static void main(String args[]) {
+
+
+		CalculadorPi programa = new CalculadorPi(INTERVALOS);
 		long inicio = System.currentTimeMillis();
-		double pi = programa.calcularPi();
+
+		double pi = programa.inicializarThreads();
 		long fin = System.currentTimeMillis();
-		System.out.println("valor de pi: "+ pi);
-		System.out.println("Error referencia = "+(CalculadorPi.REFERENCIAPI - pi));
-		System.out.println("Tiempo empleado : "+(fin-inicio)+" ms");
+		System.out.println("valor de pi: " + pi);
+		System.out.println("Error referencia = " + (CalculadorPi.REFERENCIAPI - pi));
+		System.out.println("Tiempo empleado : " + (fin - inicio) + " ms");
 	}
 }
